f09b8e411c7d07cb78ad65ee3b0c0ab6f522dfa7
Jai Pandey <jai.pandey@sael.co>
2018-10-01 13:20:57 +0530

    final commit

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export const selectBook =(book) =>{
+    //selectBook is an action creator,it needs to return an action, an object with type property
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    };
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,10 @@
+export default () => {
+    return [
+        { title : 'js', pages: '48'},
+        { title : 'java', pages: '28'},
+        { title : 'react', pages: '18'},
+        { title : 'redux', pages: '38'},
+        { title : 'spring', pages: '58'},
+        { title : 'android', pages: '88'}
+    ]
+}
